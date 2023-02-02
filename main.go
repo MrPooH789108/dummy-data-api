b2d40ea5541e85086e560ccf3d509e5068fe492f
Omeir <omeirf.02@gmail.com>
2023-02-02 21:09:24 +0530

    update main.go

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
 	"github.com/gofiber/fiber/v2"
+	"github.com/omeiirr/dummy-data-api/handlers"
+	"github.com/omeiirr/dummy-data-api/helpers"
 )
+
+var fiberLambda *fiberadapter.FiberLambda
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Server is running normally")
-	})
+	app.Get("/", handlers.HealthCheck)
+	app.Get("/users", handlers.ReturnUsers)
 
-	app.Listen(":3000")
+	if helpers.IsLambda() {
+		fiberLambda = fiberadapter.New(app)
+		lambda.Start(Handler)
+	} else {
+		app.Listen(":3000")
+	}
+
+}
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return fiberLambda.ProxyWithContext(ctx, request)
 }
