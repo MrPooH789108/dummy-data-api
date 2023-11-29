ad9ea890323051512a0a922bb8cb54a906b1decb
MrPooH789108 <wasin.s@bluegreen.co.th>
2023-11-29 09:48:11 +0700

    main

--- main.go
+++ main.go
@@ -9,19 +9,30 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/omeiirr/dummy-data-api/handlers"
 	"github.com/omeiirr/dummy-data-api/helpers"
+	"github.com/DataDog/datadog-lambda-go"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	fibertrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gofiber/fiber.v2"
 )
 
 var fiberLambda *fiberadapter.FiberLambda
 
 func main() {
+	tracer.Start()
+	defer tracer.Stop()
+
+    //span,_ := tracer.StartSpanFromContext(context.Background(),"")
+    //defer span.Finish()
+
 	app := fiber.New()
+
+	app.Use(fibertrace.Middleware(fibertrace.WithServiceName("go-fiber")))
 
 	app.Get("/", handlers.HealthCheck)
 	app.Get("/users", handlers.ReturnUsers)
 
 	if helpers.IsLambda() {
 		fiberLambda = fiberadapter.New(app)
-		lambda.Start(Handler)
+		lambda.Start(ddlambda.WrapFunction(Handler, nil))
 	} else {
 		app.Listen(":3000")
 	}
